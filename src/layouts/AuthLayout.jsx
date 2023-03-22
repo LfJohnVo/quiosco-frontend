94ca9cdbe449e9858e65426dc4fe6d317f0b71eb
vojohn95 <vojohn95@gmail.com>
2023-03-21 18:50:08 -0600

    front registrar

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,16 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
     return (
-        <div>
-            auth
-            
-            <Outlet />
-        </div>
+        <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+            <img
+                src="../img/logo.png"
+                alt="logo"
+                className="max-w-xs mx-auto mb-4"
+            />
+
+            <div className="p-10 w-full">
+                <Outlet />
+            </div>
+        </main>
     )
 }
--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,5 +1,78 @@
 export default function Registro() {
   return (
-    <div>Registro</div>
+    <>
+      <h1 className="text-4xl font-black">Crea tu cuenta</h1>
+      <p>Llena el formulario</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 py-10 px-5">
+        <form>
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="name"
+            >
+              Nombre:
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Ingresa tu nombre"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="email"
+            >
+              Email:
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Ingresa tu email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="password"
+            >
+              Contraseña:
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Ingresa tu password"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="password_confirmation"
+            >
+              Repetir Contraseña:
+            </label>
+            <input
+              id="password_confirmation"
+              name="password_confirmation"
+              type="password_confirmation"
+              className="w-full border border-gray-300 p-3 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Repite password"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Crear cuenta"
+            className="w-full bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-4 rounded-md"
+          />
+        </form>
+      </div>
+    </>
   )
 }
